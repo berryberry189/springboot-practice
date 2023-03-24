63b5b4ee6851cd2018192b1c0c68b31f5c9775f2
berryberry189 <berryberry189@naver.com>
2023-03-24 18:51:34 +0900

    Feat : DispatcherServlet 적용

--- src/main/java/com/grace/springbootpractice/SpringbootPracticeApplication.java
+++ src/main/java/com/grace/springbootpractice/SpringbootPracticeApplication.java
@@ -3,53 +3,28 @@ package com.grace.springbootpractice;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class SpringbootPracticeApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// 애플리케이션 컨텍스트
-		GenericApplicationContext applicationContext = new GenericApplicationContext();
-		applicationContext.registerBean(HelloController.class); // 빈 등록
-		applicationContext.registerBean(SimpleHelloService.class); // 빈 등록
-		applicationContext.refresh(); // 컨테이너 초기화 - 빈 오브젝트 생성해줌
+        // 애플리케이션 컨텍스트
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        applicationContext.registerBean(HelloController.class); // 빈 등록
+        applicationContext.registerBean(SimpleHelloService.class); // 빈 등록
+        applicationContext.refresh(); // 컨테이너 초기화 - 빈 오브젝트 생성해줌
 
+        // TomcatServletWebServer 를 만드는 도우미 클래스 (Jetty등으로 바꾸기도 가능)
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(applicationContext))
+                    .addMapping("/*");
+        });
+        webServer.start();
 
-		// TomcatServletWebServer 를 만드는 도우미 클래스 (Jetty등으로 바꾸기도 가능)
-		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("frontcontroller", new HttpServlet() {
-				@Override
-				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-						String name = req.getParameter("name");
-
-						HelloController helloController = applicationContext.getBean(HelloController.class);
-						// 바인딩
-						String ret = helloController.hello(name);
-
-						resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-						resp.getWriter().println(ret);
-					} else {
-						resp.setStatus(HttpStatus.NOT_FOUND.value());
-					}
-
-				}
-			}).addMapping("/*");
-		});
-		webServer.start();
-
-	}
+    }
 
 }
