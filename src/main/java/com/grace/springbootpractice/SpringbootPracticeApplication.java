b62b53ac9e64f8cf5c5f2cd28a3d9924e47d572d
berryberry189 <berryberry189@naver.com>
2023-03-24 15:55:52 +0900

    Feat : Containerless 환경 실습 + 서블릿 직접 등록

--- src/main/java/com/grace/springbootpractice/SpringbootPracticeApplication.java
+++ src/main/java/com/grace/springbootpractice/SpringbootPracticeApplication.java
@@ -1,13 +1,38 @@
 package com.grace.springbootpractice;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class SpringbootPracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootPracticeApplication.class, args);
+		System.out.println("Hello Containerless Standalone Application");
+
+		// TomcatServletWebServer 를 만드는 도우미 클래스 (Jetty등으로 바꾸기도 가능)
+		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = serverFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					resp.setStatus(200);
+					resp.setHeader("Content-Type", "text/plain");
+					resp.getWriter().println("Hello Servlet");
+
+				}
+			}).addMapping("/hello");
+			// /hello 로 들어오는 요청을 HttpServlet 익명클레스가 처리 하겠다는 의미
+
+
+		});
+		webServer.start();
+
 	}
 
 }
