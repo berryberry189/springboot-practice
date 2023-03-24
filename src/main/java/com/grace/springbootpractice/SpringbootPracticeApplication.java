5f436a00f8e51d32a05bf9e71d05028c8fbccf0a
berryberry189 <berryberry189@naver.com>
2023-03-24 16:18:31 +0900

    Feat : HelloController + FrontController 매핑, 바인딩

--- src/main/java/com/grace/springbootpractice/SpringbootPracticeApplication.java
+++ src/main/java/com/grace/springbootpractice/SpringbootPracticeApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -21,20 +22,30 @@ public class SpringbootPracticeApplication {
 		// TomcatServletWebServer 를 만드는 도우미 클래스 (Jetty등으로 바꾸기도 가능)
 		ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+
+			HelloController helloController = new HelloController();
+
+			servletContext.addServlet("frontcontroller", new HttpServlet() {
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-					String name = req.getParameter("name");
+					if(req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
 
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().println("Hello " + name);
+						// 바인딩
+						String ret = helloController.hello(name);
+
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().println(ret);
+					} else if(req.getRequestURI().equals("/user")) {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					} else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+
 				}
-			}).addMapping("/hello");
-			// /hello 로 들어오는 요청을 HttpServlet 익명클레스가 처리 하겠다는 의미
-
-
+			}).addMapping("/*");
 		});
 		webServer.start();
 
