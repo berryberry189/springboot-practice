66fbd384b6bc3718ce8d510d66ce0d9a12819056
berryberry189 <berryberry189@naver.com>
2023-03-25 22:07:39 +0900

    Feat : 스프링 컨테이너로 통합

--- src/main/java/com/grace/springbootpractice/SpringbootPracticeApplication.java
+++ src/main/java/com/grace/springbootpractice/SpringbootPracticeApplication.java
@@ -3,27 +3,46 @@ package com.grace.springbootpractice;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+@Configuration
 public class SpringbootPracticeApplication {
+
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
 
     public static void main(String[] args) {
 
         // 애플리케이션 컨텍스트
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-        applicationContext.registerBean(HelloController.class); // 빈 등록
-        applicationContext.registerBean(SimpleHelloService.class); // 빈 등록
-        applicationContext.refresh(); // 컨테이너 초기화 - 빈 오브젝트 생성해줌
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 
-        // TomcatServletWebServer 를 만드는 도우미 클래스 (Jetty등으로 바꾸기도 가능)
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("dispatcherServlet",
-                            new DispatcherServlet(applicationContext))
-                    .addMapping("/*");
-        });
-        webServer.start();
+            // 초기화
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+                // TomcatServletWebServer 를 만드는 도우미 클래스 (Jetty등으로 바꾸기도 가능)
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                                    new DispatcherServlet(this))
+                            .addMapping("/*");
+                });
+                webServer.start();
+            }
+        };
+        applicationContext.register(SpringbootPracticeApplication.class);
+        applicationContext.refresh(); // 컨테이너 초기화 - 빈 오브젝트 생성해줌
 
     }
 
