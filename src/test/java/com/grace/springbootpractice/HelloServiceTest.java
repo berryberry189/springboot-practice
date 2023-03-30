a165fd382bf23d7d3b1671493ca0d485d65b9413
berryberry189 <berryberry189@naver.com>
2023-03-30 13:23:05 +0900

    Feat : 합성 어노테이션

--- src/test/java/com/grace/springbootpractice/HelloServiceTest.java
+++ src/test/java/com/grace/springbootpractice/HelloServiceTest.java
@@ -3,9 +3,26 @@ package com.grace.springbootpractice;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@UnitTest
+@interface FastUnitTest {
+
+}
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Test
+@interface UnitTest {
+
+}
 
 public class HelloServiceTest {
-    @Test
+    @FastUnitTest
     void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
