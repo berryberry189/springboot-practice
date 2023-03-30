1f4b432cdc6574f6934a1c9faf335a82000d356b
berryberry189 <berryberry189@naver.com>
2023-03-30 10:17:27 +0900

    Feat : 커스텀 MySpringApplication 생성

--- src/main/java/com/grace/springbootpractice/SpringbootPracticeApplication.java
+++ src/main/java/com/grace/springbootpractice/SpringbootPracticeApplication.java
@@ -1,12 +1,10 @@
 package com.grace.springbootpractice;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 // bean factory 방식
@@ -25,27 +23,6 @@ public class SpringbootPracticeApplication {
     }
 
     public static void main(String[] args) {
-
-        // 애플리케이션 컨텍스트
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-
-            // 초기화
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory servletWebServerFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-                WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.register(SpringbootPracticeApplication.class);
-        applicationContext.refresh(); // 컨테이너 초기화 - 빈 오브젝트 생성해줌
+        MySpringApplication.run(SpringbootPracticeApplication.class, args);
     }
-
 }
