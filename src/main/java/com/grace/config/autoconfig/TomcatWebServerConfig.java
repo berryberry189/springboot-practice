f0e51a66cfdd8b357d0a2601f18fe0662575c14e
berryberry189 <berryberry189@naver.com>
2023-04-03 23:19:06 +0900

    Feat : Condition 조건 수정

--- src/main/java/com/grace/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/grace/config/autoconfig/TomcatWebServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
 @Conditional(TomcatWebServerConfig.TomcatCondition.class)
@@ -21,7 +22,8 @@ public class TomcatWebServerConfig {
     static class TomcatCondition implements Condition {
         @Override
         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return false;
+            // 클래스 관련 Util, 지정한 이름의 클래스가 존재 하는지 체크
+            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat", context.getClassLoader());
         }
     }
 }
