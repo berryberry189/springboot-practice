4683c6e2618772819a42c971f33ada4b9b420da0
berryberry189 <berryberry189@naver.com>
2023-04-03 23:26:45 +0900

    Feat : 커스텀 @Conditional

--- src/main/java/com/grace/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/grace/config/autoconfig/TomcatWebServerConfig.java
@@ -1,29 +1,17 @@
 package com.grace.config.autoconfig;
 
+import com.grace.config.ConditionalMyOnClass;
 import com.grace.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 
 @MyAutoConfiguration
-@Conditional(TomcatWebServerConfig.TomcatCondition.class)
+@ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
-    }
-
-    static class TomcatCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            // 클래스 관련 Util, 지정한 이름의 클래스가 존재 하는지 체크
-            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat", context.getClassLoader());
-        }
     }
 }
