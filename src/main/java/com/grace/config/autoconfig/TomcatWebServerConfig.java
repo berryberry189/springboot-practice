cc8681605fa620dd47e233c81e992003786ab7fb
berryberry189 <berryberry189@naver.com>
2023-04-05 06:52:18 +0900

    Feat : 자동구성정보 대체, @ConditionalOnMissingBean

--- src/main/java/com/grace/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/grace/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package com.grace.config.autoconfig;
 
 import com.grace.config.ConditionalMyOnClass;
 import com.grace.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean // 같은 타입의 빈을 개발자가 구성한 것이 있는 경우에는 그것을 사용하고 없다면 이것을 사용
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
