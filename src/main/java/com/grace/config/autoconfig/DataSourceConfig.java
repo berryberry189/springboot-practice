03361766916503c0e0081f30d4077258ad19095f
berryberry189 <berryberry189@naver.com>
2023-04-10 07:03:06 +0900

    Feat : jdbcTemplate, 트랜잭션 매니저 구성

--- src/main/java/com/grace/config/autoconfig/DataSourceConfig.java
+++ src/main/java/com/grace/config/autoconfig/DataSourceConfig.java
@@ -6,8 +6,12 @@ import com.grace.config.EnableMyConfigurationProperties;
 import com.grace.config.MyAutoConfiguration;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.sql.Driver;
@@ -15,6 +19,7 @@ import java.sql.Driver;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.springframework.jdbc.core.JdbcOperations")
 @EnableMyConfigurationProperties(MyDataSourceProperties.class)
+@EnableTransactionManagement
 public class DataSourceConfig {
 
     @Bean
@@ -42,6 +47,19 @@ public class DataSourceConfig {
         return dataSource;
     }
 
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class) // DataSource 타입 빈이 딱하나만 등록 되어 있다면 가져와서 사용하겠다
+    @ConditionalOnMissingBean
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    @ConditionalOnSingleCandidate(DataSource.class)
+    @ConditionalOnMissingBean
+    JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+        return new JdbcTransactionManager(dataSource);
+    }
 
 
 }
--- src/test/java/com/grace/springbootpractice/DataSourceConfig.java
+++ src/test/java/com/grace/springbootpractice/DataSourceConfig.java
@@ -1,19 +1,13 @@
 package com.grace.springbootpractice;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = SpringbootPracticeApplication.class) // 모든 빈 구성정보 끌어옴
-@TestPropertySource("classpath:/application.properties")
+@HelloBootTest
 public class DataSourceConfig {
     @Autowired
     DataSource dataSource;
